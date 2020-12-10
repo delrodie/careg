982ef0bc8107752ebfd427ece68771d7fb60ee1f
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-12-10 18:11:17 +0000

    Backend Layout

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DashboardController
+ * @Route("/backend/dashboard")
+ */
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/", name="backend_dashboard_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
